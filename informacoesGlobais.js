9639503fee42c9a90c65cb096447d9c72e312c93
cleberprestesoliveira <107960108+cleberprestesoliveira@users.noreply.github.com>
2024-10-08 20:54:29 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -4,7 +4,7 @@ async function vizualizarInformacoesGlobais(){
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas/1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo/1e9)
-    const horas = perseInt(dados.tempo_medio)
+    const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio-horas)*100)
     const porcentagemConectada = ((pessoasConectadas/ pessoasNoMundo)*100).toFixed(2)
    
