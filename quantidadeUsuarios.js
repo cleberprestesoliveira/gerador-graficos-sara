75e8790c9767f39b77dabb8a41ab5224414f47df
cleberprestesoliveira <107960108+cleberprestesoliveira@users.noreply.github.com>
2024-10-08 20:56:11 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,7 +1,7 @@
 import * as commonJS from "./common.js"
 
 async function quantidadeUsuariosPorRede(){
-    const url = 'https://raw.githubcontent.com/guilhermeonrails/api/main/numero-usuario.json'
+    const url = 'https://raw.githubcontent.com/guilhermeonrails/api/main/numero-usuarios.json'
 const res = await fetch(url)
 const dados = await res.json()
 const nomeDasRedes = Object.keys(dados)
